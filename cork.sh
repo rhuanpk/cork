96c65ec5c9d8b4ed7568a9f1000dc809b721f796
Rhuan Patriky <84540130+rhuan-pk@users.noreply.github.com>
2021-11-14 13:57:10 -0300

    update version to 3.0.0

--- cork.sh
+++ cork.sh
@@ -74,7 +74,7 @@ shift $((OPTIND - 1))
 #
 # ===========================================================
 
-readonly version="2.4.4"
+readonly version="3.0.0"
 
 array_program=("CHROME" "VS-CODE" "DISCORD" "FILEZILLA" "ANYDESK" "POSTMAN" "MY-SQL/WORKBENCH" "SIMPLESCREENRECORDER" "FLAMESHOT" "KOLOURPAINT" "NPM")
 
